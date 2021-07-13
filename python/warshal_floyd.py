d795595742056cde87fcdba1e6f723b74c24ec8b
kotatsumuri <kotatsuari@gmail.com>
2021-07-14 01:25:14 +0900

    fix warshal_floyd

--- python/warshal_floyd.py
+++ python/warshal_floyd.py
@@ -1,6 +1,8 @@
 INF = pow(10, 9)+7
 G = [[INF]*(n+1) for i in range(n+1)]
 dis = [[INF]*(n+1) for i in range(n+1)]
+
+# O(V^3)
 def warshall_floyd():
     for i in range(1, n+1): dis[i][i] = 0
     for k in range(0, n+1):
