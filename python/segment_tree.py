320931054c6afcff0ef1abf6e03fddf1bd23f64f
kotatsuari <kotatsuari@gmail.com>
2021-12-17 23:41:30 +0900

    fix segment_tree

--- python/segment_tree.py
+++ python/segment_tree.py
@@ -1,4 +1,8 @@
-class SegTree:
+# 普通のセグ木（一点更新）
+from _typeshed import IdentityFunction
+
+
+class SegTree:  # func で抽象化
     def __init__(self, a_list, func, ide_ele):
         n = len(a_list)
         self.func = func # 区間で行う操作関数
@@ -34,3 +38,91 @@ class SegTree:
 # 　　　　　　　〃　 　　　　　　上のブロックの番号は (i-1)//2
 # 
 # ide_ele は単位元　その値によって結果が変わらないもの（ min なら INF）
+
+
+# 遅延セグ木（区間更新）
+from collections import deque
+class SegTree:  # func で抽象化
+    def __init__(self, a_list, func, ide_ele):
+        n = len(a_list)
+        self.func = func # 区間で行う操作関数
+        self.ide_ele = ide_ele # 単位元
+        self.num = 1 << (n-1).bit_length()
+        self.dat = [ide_ele]*2*self.num
+        self.lazy = [ide_ele]*2*self.num
+        for i in range(0, n): self.dat[self.num-1 + i] = a_list[i]
+        for i in range(self.num-2, -1, -1):
+            self.dat[i] = self.func(self.dat[2*i+1], self.dat[2*i+2])
+
+    def eval(self, k):
+        if(self.lazy[k] == self.ide_ele): return 
+        if(k < self.num-1):
+            self.lazy[k*2 + 1] = self.lazy[k]
+            self.lazy[k*2 + 2] = self.lazy[k]
+        self.dat[k] = self.lazy[k]
+        self.lazy[k] = self.ide_ele
+
+    # def update_sub(self, a, b, x, k, l, r):
+    #     self.eval(k)
+    #     if(a <= l and r <= b):
+    #         self.lazy[k] = x
+    #         self.eval(k)
+    #     elif(a < r and l < b):
+    #         self.update_sub(a, b, x, k*2 + 1, l, (l+r)//2)
+    #         self.update_sub(a, b, x, k*2 + 2, (l+r)//2, r)
+    #         self.dat[k] = self.func(self.dat[k*2 + 1], self.dat[k*2 + 2])
+
+    def update_sub(self, a, b, x, k, l, r):
+        d = deque()
+        d.append((-1, a, b, x, k, l, r))
+        d.append(( 1, a, b, x, k, l, r))
+        while(d):
+            t,a,b,x,k,l,r = d.pop()
+            if(t > 0): 
+                self.eval(k)
+                if(a <= l and r <= b):
+                    self.lazy[k] = x
+                    self.eval(k)
+                elif(a < r and l < b):
+                    d.append((-1, a, b, x, k*2 + 2, (l+r)//2, r))
+                    d.append(( 1, a, b, x, k*2 + 2, (l+r)//2, r))
+                    d.append((-1, a, b, x, k*2 + 1, l, (l+r)//2))
+                    d.append(( 1, a, b, x, k*2 + 1, l, (l+r)//2))
+            else:
+                if(a <= l and r <= b): pass
+                elif(a < r and l < b):
+                    self.dat[k] = self.func(self.dat[k*2 + 1], self.dat[k*2 + 2])
+
+    def update(self, a, b, x): # 半開区間 [a, b) の値を xに更新
+        self.update_sub(a, b, x, 0, 0, self.num)
+
+    def query_sub(self, a, b, k, l, r):
+        d = deque()
+        # d.append((-1, a, b, k, l, r))
+        d.append(( 1, a, b, k, l, r))
+        ret = self.ide_ele
+        while(d):
+            t,a,b,k,l,r = d.pop()
+            if(t > 0):
+                self.eval(k)
+                if(r <= a or b <= l): ret = self.func(ret, self.ide_ele)
+                elif(a <= l and r <= b): ret = self.func(ret, self.dat[k])
+                else:
+                    # d.append((-1, a, b, k*2 + 2, (l+r)//2, r))
+                    d.append(( 1, a, b, k*2 + 2, (l+r)//2, r))
+                    # d.append((-1, a, b, k*2 + 1, l, (l+r)//2))
+                    d.append(( 1, a, b, k*2 + 1, l, (l+r)//2))
+            else:
+                pass
+        return ret
+
+    def query(self, a, b): # 半開区間 [a, b) のクエリ
+        return self.query_sub(a, b, 0, 0, self.num)
+
+
+# [sample_func] 最小値を求める
+# def func(x, y):
+#     return max(x,y)
+# ide_ele = 0
+
+# [ex. Tenkei 029]
